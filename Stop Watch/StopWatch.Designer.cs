805b1f0c709100bf6f5001d5bc27e931f2f0439d
Munzir Kalim Ahmed <72119116+munzirkalimahmed@users.noreply.github.com>
2022-10-21 23:10:27 +0500

    Stop Watch

---
+++ Stop Watch/StopWatch.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace DigitalClock
+{
+    partial class StopWatch
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.LblCounter = new System.Windows.Forms.Label();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.btnReset = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // LblCounter
+            // 
+            this.LblCounter.AutoSize = true;
+            this.LblCounter.Font = new System.Drawing.Font("Segoe UI", 26F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.LblCounter.Location = new System.Drawing.Point(329, 142);
+            this.LblCounter.Name = "LblCounter";
+            this.LblCounter.Size = new System.Drawing.Size(266, 70);
+            this.LblCounter.TabIndex = 0;
+            this.LblCounter.Text = "Counter: 0";
+            this.LblCounter.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(172, 318);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(188, 61);
+            this.btnStart.TabIndex = 1;
+            this.btnStart.Text = "Start Count Down";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btnStop
+            // 
+            this.btnStop.Location = new System.Drawing.Point(385, 318);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(188, 61);
+            this.btnStop.TabIndex = 2;
+            this.btnStop.Text = "Stop Count Down";
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // btnReset
+            // 
+            this.btnReset.Location = new System.Drawing.Point(598, 318);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(182, 61);
+            this.btnReset.TabIndex = 3;
+            this.btnReset.Text = "Reset CountDown";
+            this.btnReset.UseVisualStyleBackColor = true;
+            this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Counter
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(957, 522);
+            this.Controls.Add(this.btnReset);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.LblCounter);
+            this.Name = "Counter";
+            this.Text = "Counter";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Label LblCounter;
+        private Button btnStart;
+        private Button btnStop;
+        private Button btnReset;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
---
+++ Stop Watch/StopWatch.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DigitalClock
+{
+    public partial class StopWatch : Form
+    {
+        public StopWatch()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            timer1.Start();
+        }
+        int counter = 0;
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            LblCounter.Text = "Counter: " + counter.ToString() + " s";
+            counter++;
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            timer1.Stop();
+        }
+
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+            counter = 0;
+        }
+    }
+}
